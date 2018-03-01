48c79b14830a320e97d6ee5fb087b104eb711138
dimakorzhovnik <korzhovnik.dima@gmail.com>
2018-03-01 19:40:24 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function solveSudoku(matrix) {
-  // your solution
-}
+  var  num = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+  var result = [];
+
+  for (let row = 0; row < matrix.length; row++) {
+      for (let col = 0; col <  matrix[row].length; col++) {
+          if (matrix[row][col] === 0) {
+              let num = count(row, col);
+              for (let candidat of num) {
+                  matrix[row][col] = candidat;
+               solveSudoku(matrix);
+                
+              }
+            
+          }
+      }
+  
+    }
+
+  function count(row, col) {
+
+      row =  Math.floor(row/3)*3;
+      col =  Math.floor(col/3)*3;
+      for (let i = 0; i < 9; i++) {
+          result.push([matrix[row][i], matrix[i][col], matrix[row + i % 3][col + Math.floor(i / 3)]])
+      }
+      return result;
+  }
+  return matrix;
+};
